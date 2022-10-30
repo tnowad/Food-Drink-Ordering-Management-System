dff88d94da375b970af314936c41d004ceb47f66
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2022-10-30 15:02:17 +0700

    Account

---
+++ src/Account.java
@@ -0,0 +1,30 @@
+public class Account {
+    private int id;
+    private String acc, pass;
+    public Account(int id, String acc, String pass) {
+        this.id = id;
+        this.acc = acc;
+        this.pass = pass;
+    }
+    public Account() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getAcc() {
+        return acc;
+    }
+    public void setAcc(String acc) {
+        this.acc = acc;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
