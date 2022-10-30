94a5f93b8f20abb42e6004bf72c7b4d2a5822e92
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2022-10-30 17:32:10 +0700

    add custumer

---
+++ src/Customer.java
@@ -0,0 +1,21 @@
+
+public class Customer extends Person {
+    private int point;  
+    
+    public int getPoint() {
+        return point;
+    }
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+    public void tichDiem() {
+
+    }
+    public void sdDiem() {
+        
+    }
+    public void order() {
+        
+    }
+}
