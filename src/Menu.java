8d77898817f1fca99ea23b43011457f39ede651a
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2022-10-30 17:30:54 +0700

    add menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Menu {
-    public Scanner sc = new Scanner(System.in);
-    public static void login() {
-        
+    public static Scanner sc = new Scanner(System.in);
+    public static Account login() {
+        System.out.println("LOGIN (Y/N)");
+        char choose = sc.nextLine().charAt(0);
+        choose = Character.toLowerCase(choose);
+        if(choose == 'y')
+        {
+            // Call method login in AccountList
+            // return account
+        }
+        System.out.println(" Bạn đang dùng tài khoản khách");
+        return null; // @TODO new person
     }
 }
