a60d3094763a99641cd9791f6361b8a3b3f55f2d
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2022-10-30 17:49:04 +0700

    confit all

--- src/PaymentList.java
+++ src/PaymentList.java
@@ -1,24 +1,23 @@
-public class ProductList implements IList {
-    private int maxLength = 1000;
+public class PaymentList implements IList{
     private static int totalId = 0;
-    private Product[] array;
-    public ProductList() {
-    }
     @Override
     public Object search(int id) {
         // TODO Auto-generated method stub
         return null;
     }
+
     @Override
     public boolean append(Object obj) {
         // TODO Auto-generated method stub
         return false;
     }
+
     @Override
     public boolean remove(int id) {
         // TODO Auto-generated method stub
         return false;
     }
+
     @Override
     public boolean change(int id) {
         // TODO Auto-generated method stub
