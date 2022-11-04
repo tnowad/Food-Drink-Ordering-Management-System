e257486236357b038e500bf3721aac37a9a275d6
BuiBao3103 <hongbao2003@gmail.com>
2022-11-04 15:35:44 +0700

    fix product List

--- src/ProductList.java
+++ src/ProductList.java
@@ -1,36 +1,6 @@
 import java.util.ArrayList;
 
 public class ProductList extends ArrayList {
-    private int maxLength = 1000;
     private static int totalId = 0;
-    private Product[] array;
-    public ProductList() {
-    }
-    
-    @Override
-    public Object search(int id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
-    @Override
-    public boolean append(Object obj) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean remove(int id) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean change(int id) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-    public Person login(int id) {
-        return null;
-    }
 }
