98db5dea30d683c83d57dcbce01f3742bfbe6a33
BuiBao3103 <hongbao2003@gmail.com>
2022-11-04 16:03:24 +0700

    create menu customer

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,28 +1,32 @@
 import java.util.Arrays;
 
-public abstract class ArrayList implements IList{
+public abstract class ArrayList implements IList {
     private Object[] array;
     private int total;
     {
         total = 0;
         array = new ArrayList[100];
     }
-    ArrayList(){
+
+    ArrayList() {
     }
+
     /**
      * Add new element to the end of array
      * if successfully, return true
+     *
      * @param obj
      * @return boolean
      */
-    public boolean append(Object obj) { 
+    public boolean append(Object obj) {
         try {
             array[total] = obj;
         } catch (IndexOutOfBoundsException e) {
-            array = Arrays.copyOf(array, total+1);
+            array = Arrays.copyOf(array, total + 1);
             array[total] = obj;
-            if(array[total] != null) total++;
-        } catch(Exception e) {
+            if (array[total] != null)
+                total++;
+        } catch (Exception e) {
             return false;
         }
         return true;
--- src/Menu.java
+++ src/Menu.java
@@ -2,16 +2,38 @@ import java.util.Scanner;
 
 public class Menu {
     public static Scanner sc = new Scanner(System.in);
+
     public static Account login() {
         System.out.println("LOGIN (Y/N)");
         char choose = sc.nextLine().charAt(0);
         choose = Character.toLowerCase(choose);
-        if(choose == 'y')
-        {
+        if (choose == 'y') {
             // Call method login in AccountList
             // return account
         }
         System.out.println(" Bạn đang dùng tài khoản khách");
         return null; // @TODO new person
+    }
+
+    /**
+     * 
+     * @return int
+     */
+    public static int menuCustomer() {
+        /**
+         * TODO:
+         * [x] display customer function list
+         * [x] get customer choice
+         * [x] return customer choice
+         */
+        System.out.println("1. Hiện danh sách sản phẩm.");
+        System.out.println("2. Thông tin người dùng.");
+        System.out.println("0. Thoát.");
+        int choice = -1;
+        while (choice < 0 || choice > 2) {
+            System.out.print("Lựa chọn: ");
+            choice = Integer.parseInt(sc.nextLine());
+        }
+        return choice;
     }
 }
