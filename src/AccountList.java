d23bcc8cc78cb61808d85c339c0eb530e5c398be
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-04 15:27:45 +0700

    add comment

--- src/AccountList.java
+++ src/AccountList.java
@@ -1,16 +1,9 @@
-public class AccountList extends ArrayList implements IList {
-    private static int totalId = 0;
+public class AccountList extends ArrayList {
 
     @Override
     public Object search(int id) {
         // TODO Auto-generated method stub
         return null;
-    }
-
-    @Override
-    public boolean append(Object obj) {
-        // TODO Auto-generated method stub
-        return false;
     }
 
     @Override
@@ -23,8 +16,5 @@ public class AccountList extends ArrayList implements IList {
     public boolean change(int id) {
         // TODO Auto-generated method stub
         return false;
-    }
-    public Person login(int id) {
-        return null;
     }
 }
