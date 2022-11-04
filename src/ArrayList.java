2b01228947f7449e881a738c62f5675a62cbce9d
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-04 15:01:31 +0700

    edit arraylist

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,15 +1,28 @@
+import java.util.Arrays;
+
 public abstract class ArrayList {
     private Object[] array;
-    private int length;
+    private int total;
     private int maxLength;
-
+    {
+        total = 0;
+        maxLength = 100;
+    }
+    ArrayList(){
+        array = new ArrayList[maxLength];
+    }
     public abstract Object search(int id);
 
-    public abstract boolean append(Object obj);
-
-    public abstract boolean remove(int id);
+    public boolean append(Object obj) {
+        try {
+            array[total] = obj;
+        } catch (IndexOutOfBoundsException e) {
+            array = Arrays.copyOf(array, total+1);
+        }
+        return false;
+    };
 
     public abstract boolean change(int id);
 
-    public abstract Person login(int id);
+    public abstract boolean remove(int id);
 }
