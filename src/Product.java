35846ad3ea08e5d20950ed6d1647408b4e1c3010
Nguyen Minh Tuan <tnowad@gmail.com>
2022-11-09 23:25:53 +0700

    refactor all class

--- src/Product.java
+++ src/Product.java
@@ -4,64 +4,77 @@ public class Product {
     private int id;
     private String name;
     private int price;
-    private String manufacturer;
+    private int count;
+    private String brand;
     private Date MFG;
     private Date EXP;
+
     public Product() {
     }
-    public Product(int id, String name, int price, String manufacturer, Date mFG, Date eXP) {
+
+    public Product(int id, String name, int price, int count, String brand, Date MFG, Date EXP) {
         this.id = id;
         this.name = name;
         this.price = price;
-        this.manufacturer = manufacturer;
-        MFG = mFG;
-        EXP = eXP;
+        this.count = count;
+        this.brand = brand;
+        this.MFG = MFG;
+        this.EXP = EXP;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getPrice() {
         return price;
     }
+
     public void setPrice(int price) {
         this.price = price;
     }
-    public String getManufacturer() {
-        return manufacturer;
+
+    public int getCount() {
+        return count;
     }
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
+
+    public void setCount(int count) {
+        this.count = count;
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public Date getMFG() {
         return MFG;
     }
+
     public void setMFG(Date MFG) {
         this.MFG = MFG;
     }
+
     public Date getEXP() {
         return EXP;
     }
+
     public void setEXP(Date EXP) {
         this.EXP = EXP;
     }
-    @Override
-    public String toString() {
-        return "Product [id=" + id + ", name=" + name + ", price=" + price + ", manufacturer=" + manufacturer + ", MFG="
-                + MFG + ", EXP=" + EXP + "]";
-    }
-    /**
-     * @phuonganhtq
-     * TODO:
-     * [ ] create input method for init this class
-     * [ ] create main to check and print test class
-     */
 }
