205266eca56127c5e5f6a02884b37895ac350d05
omodtung <103139755+omodtung@users.noreply.github.com>
2022-11-10 13:55:21 +0700

    search find dis play productlist

--- src/ProductList.java
+++ src/ProductList.java
@@ -5,10 +5,49 @@ public class ProductList extends ArrayList {
     ProductList(ArrayList arrayList) {
         super(arrayList);
     }
+
+    public Object find(int id )
+    {
+        for ( Object object : array )
+        {
+            if ( ((Product) object ).getId()==id)
+            {
+                return object;
+
+            }
+        }
+        return null;
+
+    }
+    public Object search(String string)
+    {
+        for ( Object object : array)
+        {
+            if (( (Product)object).getName().indexOf(string )!=-1)
+            {
+                return object ;
+            }
+        }
+        return null;
+    }
+
+    public Object remove(  int id   )
+    {
+        for ( Object object : array )
+        {
+            if ( ((Product) object).getId()==id)
+            {
+            }
+        }
+}
     public void display(){
         /**
          * @TODO:
          * [ ] Show list product
          */
+        for (Object object : array)
+        {
+            System.out.println ( object.toString());
+        }
     }
 }
