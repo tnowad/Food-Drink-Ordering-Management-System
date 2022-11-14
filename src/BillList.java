a3e1490e4097dcc8139f18101ec036d3f08e3fbb
BuiBao3103 <hongbao2003@gmail.com>
2022-11-14 08:30:03 +0700

    format display billList

--- src/BillList.java
+++ src/BillList.java
@@ -23,12 +23,18 @@ public class BillList extends ArrayList {
 
         for (Object object : array) {
             System.out.println(
-                    String.format("│%-4s│%-4s│%-4s│%-4s│%-5s│%-5s│", ((Bill) object).getId(),
+                    String.format("│%-4d│%-4d│%-4d│%-4d│%-5d│%-5d│", ((Bill) object).getId(),
                             ((Bill) object).getIdCustomer(),
                             ((Bill) object).getIdSalesman(),
-                            ((Bill) object).getIdCustomer(),
-                            ((Bill) object).getIdCustomer(),
+                            ((Bill) object).getIdProduct()[0],
+                            ((Bill) object).getAmount()[0],
                             ((Bill) object).getPoint()));
+            for (int i = 1; i < ((Bill) object).getIdProduct().length; i++) {
+                System.out.println(
+                        String.format("│%-4s│%-4s│%-4s│%-4d│%-5d│%-5s│", "", "", "", ((Bill) object).getIdProduct()[0],
+                                ((Bill) object).getAmount()[0], ""));
+
+            }
         }
         System.out.println("├────┴────┴────┴────┴─────┴─────┤");
 
