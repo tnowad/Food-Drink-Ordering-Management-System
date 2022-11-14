f4c3bed8a8766e3bb834d16290ba28f4d5dcb341
BuiBao3103 <hongbao2003@gmail.com>
2022-11-14 07:51:24 +0700

    format bill display

--- src/BillList.java
+++ src/BillList.java
@@ -1,3 +1,5 @@
+import java.text.SimpleDateFormat;
+
 public class BillList extends ArrayList {
 
     public BillList() {
@@ -5,5 +7,30 @@ public class BillList extends ArrayList {
 
     public BillList(ArrayList arrayList) {
         super(arrayList);
+    }
+
+    public void display() {
+        /**
+         * @TODO:
+         *        [x] Show list product
+         */
+        // Bill [id='0', idCustomer='0', idSalesman='0', idProduct='[1, 2, 3]',
+        // amount='[3, 4, 5]', point='7']
+        System.out.println("├────┬────┬────┬────┬─────┬─────┤");
+        System.out.println(
+                String.format("│%-4s│%-4s│%-4s│%-4s│%-5s│%-5s│", "id", "id KH", "id NV", "id SP", "SL", "Điểm"));
+        System.out.println("├────┼────┼────┼────┼─────┼─────┤");
+
+        for (Object object : array) {
+            System.out.println(
+                    String.format("│%-4s│%-4s│%-4s│%-4s│%-5s│%-5s│", ((Bill) object).getId(),
+                            ((Bill) object).getIdCustomer(),
+                            ((Bill) object).getIdSalesman(),
+                            ((Bill) object).getIdCustomer(),
+                            ((Bill) object).getIdCustomer(),
+                            ((Bill) object).getPoint()));
+        }
+        System.out.println("├────┴────┴────┴────┴─────┴─────┤");
+
     }
 }
